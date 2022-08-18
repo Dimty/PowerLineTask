b60aee1690b5183da64237dc355e8de1df30103a
Dimty <nikitin-dmitriyy@mail.ru>
2022-08-18 15:56:03 +0600

    SQL task.

---
+++ PowerLineTask/SQLQuery.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE Customers
+(
+    Id   INT IDENTITY(1,1),
+    Name nvarchar(15)
+);
+
+INSERT 
+Customers VALUES ('Max');
+INSERT
+Customers VALUES ('Pavel');
+INSERT
+Customers VALUES ('Ivan');
+INSERT
+Customers VALUES ('Leonid');
+
+CREATE TABLE Orders
+(
+    Id         INT IDENTITY(1,1),
+    CustomerId INT
+);
+
+INSERT
+Orders VALUES (2);
+INSERT
+Orders VALUES (4);
+
+
+SELECT Name AS Customers
+FROM ['DBName'].[Customers]
+WHERE Customers.Id NOT IN (SELECT CustomerId FROM ['DBName'].[Orders])
